e682d93c1e0e4bdc40352a18f0e9c3dbc48ba96b
Md Akther Hosen <dev.aktherhosen@gmail.com>
2025-01-15 23:13:16 +0600

    update

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // await client.close();
   }
 }
 run().catch(console.dir);
